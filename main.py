ee127631f43bd8093a58d7db1f9b1d7f89c602e6
Peter <pandreoli@ryerson.ca>
2017-09-27 22:31:26 -0400

    Added basic card searching. Only works with names.

--- main.py
+++ main.py
@@ -8,6 +8,32 @@ from mtgsdk import Supertype
 from mtgsdk import Subtype
 from mtgsdk import Changelog
 
+
+
+#Parses the message and finds all requests for cards.
+#message - variable that holds the message that is to be parsed
+
+#card_library = Card.all()
+def find_card_request (message):
+    msg = message.content
+    card_requests = []
+    parsing = True
+    while parsing:
+        opening_brackets = msg.find('[[')
+        closing_brackets = msg.find(']]')
+        if opening_brackets == -1 or closing_brackets == -1:
+            print("done with request")
+            break
+        if closing_brackets < opening_brackets:
+            break
+        curr_parse = "\""+msg[opening_brackets+2:closing_brackets]+"\""
+        card_requests.append(curr_parse)
+        print(curr_parse)
+        msg = msg[closing_brackets+2:]
+        #print("new request")
+        #print(msg)
+    return card_requests
+
 client = discord.Client()
 @client.event
 async def on_ready():
@@ -18,25 +44,27 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    requests = []
+    if message.content.startswith("good bot"):
+        await client.send_message(message.channel, "Thanks :blush: :heart:")
     #Check if message has potential for  having a card request. Done by checking the number of open and close brackets
-    if message.content.count('[[') == message.content.count(']]'):
-        find_card_request(message)
+    if message.content.count('[[') == message.content.count(']]') and message.content.count('[[') != 0:
+        #Parse the input, find all the card requests
+        requests = find_card_request(message)
+        await client.send_message(message.channel, 'Request received...')
+        for card in requests:
+            temp = Card.where(name=card).all()
+            if len(temp) == 0:
+                await client.send_message(message.channel,"The card " + card + "cant be found.")
+            else:
+                set = 0
+                await client.send_message(message.channel,  "**\""+temp[set].name + "\"**\n" + temp[set].mana_cost + "\n"+
+                                          temp[set].type + "\n"+ temp[set].rarity + "\n"+ temp[set].text + "\n\n"+
+                                          temp[set].image_url)
+
+
+
+
 
 
 client.run('MzYyNDIyNzg0MzUxMTQxODg5.DKycFw.cEEp9aDXGD_FZ_tBXs64mrLw9dU')
-
-#Parses the message and finds all requests for cards.
-#message - variable that holds the message that is to be parsed
-def find_card_request (message):
-    msg = message.content
-    card_requests = []
-    parsing = True
-    while parsing:
-        opening_brackets = msg.find('[[')
-        closing_brackets = msg.find(']]')
-        if opening_brackets == -1 or closing_brackets == -1:
-            break
-        curr_parse = msg[opening_brackets+1:closing_brackets]
-        card_requests.append(curr_parse)
-        msg = msg[closing_brackets+1:-1]
-#test
