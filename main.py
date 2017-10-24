c903a8a56d40aeafaa5e2f2523652e094732f66d
Peter <pandreoli@ryerson.ca>
2017-10-23 22:37:58 -0400

    Modularized Card Fetching Function

--- main.py
+++ main.py
@@ -112,6 +112,16 @@ def create_config():
               "Foreign_Names: False\n")
 
 
+async def fluff_messages(message):
+    if "good bot" in message.content or "Good bot" in message.content:
+        await client.send_message(message.channel, "Thanks :smile: :heart:")
+    if "bad bot" in message.content or "Bad bot" in message.content:
+        await client.send_message(message.channel, "Contact my dev, I can only do as well as he programs :cry:")
+    if "send nudes" in message.content or "Send nudes" in message.content:
+        await client.send_message(message.channel, "You can see whats hiding underneath here :wink: \nhttps://github.co"
+                                                   "m/topfight/DiscordGathererBot")
+
+
 def create_config(file_name):
     """
     Creates a configuration file.
@@ -160,7 +170,36 @@ def create_config(file_name):
               "Foreign_Names: False\n")
 
 
-async def fetch_card(requests, message):
+async def card_fetch_line_of_defense_two(card_name, channel, card_list):
+    """
+    This function is called when wanting to get the card details. But first you need to make sure the search for the
+    card yielded a valid result.
+
+    :param: card_name: String holding the name of the card to search.
+    :param: channel: Channel object to send the message to.
+    :param: card_list: List of Card objects.
+    :return:
+    """
+    print(card_name)
+    # If the list is empty then that means the card could not be found.
+    if len(card_list) == 0:
+        # Try searching without the quotes. This will look for a card with (supposedly) the closest name.
+        card_list = Card.where(name=card_name[1:-1]).all()
+        # If the list has contents then a card was found.
+        if len(card_list) > 0:
+            # Tell the user it could not find the card and that this is the best guess.
+            await client.send_message(channel, "The card " + card_name + "cant be found. This is the closest "
+                                      + "I could find... sorry :frowning:")
+    # If the list is empty then that means the card could not be found.
+    if len(card_list) == 0:
+        # Tell the user that the card could not be found.
+        await client.send_message(channel, "The card " + card_name + "cant be found."
+                                  + "\nIf you believe this is an error please email " + support_email)
+    else:
+        await get_card_details(card_list, channel)
+
+        
+async def fetch_card(requests, channel):
     """
     Uses the MTGSDK api to fetch cards with the names stored in requests. Then prints details about the card depending
     on the options turned to True in the ini file for the appropriate server.
@@ -178,58 +217,55 @@ async def fetch_card(requests, message):
             continue
         # Fetch a card with the exact name as requested. It gets the exact name by having the quotes surrounding it
         # (added previously). temp becomes a list consisting of the requested card in each set it was printed in.
-        # TODO: See if .all() is just over complicating things.
         temp = Card.where(name=card).all()
-        print(card)
-        # If the list is empty then that means the card could not be found.
-        if len(temp) == 0:
-            # Try searching without the quotes. This will look for a card with (supposedly) the closest name.
-            temp = Card.where(name=card[1:-1]).all()
-            # If the list has contents then a card was found.
-            if len(temp) > 0:
-                # Tell the user it could not find the card and that this is the best guess.
-                await client.send_message(message.channel, "The card " + card + "cant be found. This is the closest "
-                                          + "I could find... sorry :frowning:")
-        # If the list is empty then that means the card could not be found.
-        if len(temp) == 0:
-            # Tell the user that the card could not be found.
-            await client.send_message(message.channel, "The card " + card + "cant be found."
-                                      + "\nIf you believe this is an error please email " + support_email)
-        # If the list has contents the proceed with printing its data.
-        else:
-            # Loop through the list of cards.
-            for card_set in range(0, len(temp)):
-                # Try to print the desired card details. A ValueError is raised if there are any problems with getting
-                # the desired information.
-                try:
-                    print(temp[card_set])
-                    # Holds the result of the fetch.
-                    request_output = ""
-                    # cfg_parser.items("Print Options") gets the option-value pair from the ini file under the header
-                    # "Print Options".
-                    # Ex - The following would be in an ini file: card_name = True. option -> 'card_name' value -> True
-                    # TODO: Find ways to make this more modular.
-                    for option, value in cfg_parser.items("Print Options"):
-                        # If the option has been turned on, then print the detail related to this option.
-                        if value == 'True':
-                            print("Option:" + option)
-                            # gets the value of the option from the card.
-                            attr = getattr(temp[card_set], option)
-                            # Checks if the value could not be found.
-                            # TODO This will mess things up when user turns on Loyalty printing and requests a card without loyalty. Find a better way to check if an attribute is missing
-                            if attr is None or attr == "None":
-                                # Raise a value error.
-                                raise ValueError
-                            # Append the attribute to the message to be printed.
-                            request_output += str(attr)+"\n"
-                    print(request_output)
-                    # Send the message to the requester.
-                    await client.send_message(message.channel, request_output)
-                    # Stop the loop as we have successfully sent the message.
-                    break
-                # A ValueError is raised whenever there is trouble retrieving an attribute from a card.
-                except ValueError:
-                    print("Couldnt find a card at position " + str(card_set))
+        await card_fetch_line_of_defense_two(card, channel, temp)
+
+
+
+
+async def get_card_details(card_options, channel):
+    """
+    Gets the details of a card using a list of query results stored in card_options. Uses the details specified in the
+    ini file for the server.
+    :param: card_options: List of Card objects
+    :param: message: The original message sent
+    :return:
+    """
+    # Loop through the list of cards.
+    for card_set in range(0, len(card_options)):
+        # Try to print the desired card details. A ValueError is raised if there are any problems with getting
+        # the desired information.
+        try:
+            print(card_options[card_set])
+            # Holds the result of the fetch.
+            request_output = ""
+            # cfg_parser.items("Print Options") gets the option-value pair from the ini file under the header
+            # "Print Options".
+            # Ex - The following would be in an ini file: card_name = True. option -> 'card_name' value -> True
+            # TODO: Find ways to make this more modular.
+            for option, value in cfg_parser.items("Print Options"):
+                # If the option has been turned on, then print the detail related to this option.
+                if value == 'True':
+                    print("Option:" + option)
+                    # gets the value of the option from the card.
+                    attr = getattr(card_options[card_set], option)
+                    # Checks if the value could not be found.
+                    # TODO This will mess things up when user turns on Loyalty printing and requests a card without loyalty. Find a better way to check if an attribute is missing
+                    if attr is None or attr == "None":
+                        # Raise a value error.
+                        print("PROBLEM HERE")
+                        print(attr)
+                        raise ValueError
+                    # Append the attribute to the message to be printed.
+                    request_output += str(attr) + "\n"
+            print(request_output)
+            # Send the message to the requester.
+            await client.send_message(channel, request_output)
+            # Stop the loop as we have successfully sent the message.
+            break
+        # A ValueError is raised whenever there is trouble retrieving an attribute from a card.
+        except ValueError:
+            print("Couldnt find a card at position " + str(card_set))
 
 
 async def open_config(server):
@@ -288,14 +324,14 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if "good bot" in message.content:
-        await client.send_message(message.channel, "Thanks :smile: :heart:")
+    await fluff_messages(message)
+
     # Check if message has potential for  having a card request. Done by checking the number of open and close brackets
     if message.content.count('[[') > 0 and message.content.count(']]') > 0:
         # Parse the input, find all the card requests
         requests = find_card_request(message)
         await client.send_message(message.channel, 'Request received...')
-        await fetch_card(requests, message)
+        await fetch_card(requests, message.channel)
 
 
 def on_exit():
